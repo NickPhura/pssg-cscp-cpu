259ff9d6ba2489125ff33fed761623541ee05413
Nick Phura <nickphura@gmail.com>
2025-04-14 10:59:30 -0700

    - Attempt to fix karma tests. Add Karma deprecation TODO.

--- cpu-app/ClientApp/src/test.ts
+++ cpu-app/ClientApp/src/test.ts
@@ -1,13 +1,6 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+// TODO: Karma is deprecated and should be replaced. See deprecation note: https://www.npmjs.com/package/karma.
 
 import "zone.js/testing";
-// TODO NICK: Remove based on upgrade instructions https://angular.dev/update-guide?v=16.0-17.0&l=3
-// import 'zone.js/dist/long-stack-trace-zone';
-// import 'zone.js/dist/proxy.js';
-// import 'zone.js/dist/sync-test';
-// import 'zone.js/dist/jasmine-patch';
-// import 'zone.js/dist/async-test';
-// import 'zone.js/dist/fake-async-test';
 import { getTestBed } from "@angular/core/testing";
 import {
   BrowserDynamicTestingModule,
