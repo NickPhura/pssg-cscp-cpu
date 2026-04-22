338d693a12c104a21ab705fa5a2fb33a8dc3dc72
Fintan Roche <Fintan.Roche@quartech.com>
2026-04-22 14:25:32 -0700

    new user mapping refactored

--- cpu-app/ClientApp/src/app/authenticated/new-user/new-user.component.ts
+++ cpu-app/ClientApp/src/app/authenticated/new-user/new-user.component.ts
@@ -42,7 +42,6 @@ export class NewUserComponent implements OnInit {
       let userSettings = this.stateService.userSettings.getValue();
       this.trans.organizationId = userSettings.accountId;
       this.trans.userId = userSettings.userId;
-      this.trans.serviceProvider = undefined;
 
       if (!this.trans.hasRequiredFields()) {
         this.notificationQueueService.addNotification(
